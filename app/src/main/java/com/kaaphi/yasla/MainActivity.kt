e73da666c7b794e2c6f722dec484af866eca66eb
phil kaasa <phil@kaaphi.com>
2022-12-11 10:59:20 -0600

    Implement item quantity and add focus request for adding items.

--- app/src/main/java/com/kaaphi/yasla/MainActivity.kt
+++ app/src/main/java/com/kaaphi/yasla/MainActivity.kt
@@ -14,12 +14,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,6 +32,7 @@ import androidx.navigation.compose.rememberNavController
 import com.kaaphi.yasla.model.ListItem
 import com.kaaphi.yasla.model.ShoppingListState
 import com.kaaphi.yasla.ui.theme.YaslaTheme
+import kotlinx.coroutines.delay
 import org.burnoutcrew.reorderable.ReorderableItem
 import org.burnoutcrew.reorderable.detectReorder
 import org.burnoutcrew.reorderable.rememberReorderableLazyListState
@@ -63,6 +64,20 @@ fun ListApp(
         startDestination = ShoppingListScreen.List.name
     ) {
         composable(route = ShoppingListScreen.List.name) {
+            val editItem: ListItem? by viewModel.editItemState.collectAsState()
+            editItem?.let { item ->
+                EditQuantity(
+                    onChangeQuantity = { quantity ->
+                        viewModel.updateItem(item) {
+                            copy(quantity = quantity)
+                        }
+                        viewModel.editItem(null)
+                    },
+                    onDismiss = {
+                        viewModel.editItem(null)
+                    })
+            }
+
             Column {
                 ShoppingList(modifier = Modifier.weight(1f),
                     list = viewModel.list,
@@ -71,7 +86,8 @@ fun ListApp(
                         viewModel.updateItem(this) {
                             copy(isChecked = isChecked)
                         }
-                    }
+                    },
+                    onEditItemClicked = viewModel::editItem
                 )
                 BottomBar(
                     onClearCheckedItemsClicked = {
@@ -94,7 +110,7 @@ fun ListApp(
 
 @Composable
 fun ListItemRow(item: ListItem, modifier: Modifier = Modifier, reorderModifier: Modifier = Modifier,
-                onCheckedChange: (Boolean) -> Unit, ) {
+                onCheckedChange: (Boolean) -> Unit, onEditItemClicked: (ListItem) -> Unit) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
@@ -105,9 +121,11 @@ fun ListItemRow(item: ListItem, modifier: Modifier = Modifier, reorderModifier: 
         } else {
             TextDecoration.None
         }
-        Text(item.name, textDecoration = decoration)
+        Text("${item.quantity?.let{"$it "} ?: ""}${item.name}", textDecoration = decoration)
         Spacer(modifier = Modifier.weight(1f))
-        FilledIconButton(onClick = { /* TODO */}) {
+        FilledIconButton(onClick = {
+            onEditItemClicked.invoke(item)
+        }) {
             Icon(Icons.Default.Edit, contentDescription = "Edit")
         }
         FilledIconButton(onClick = {}, modifier = reorderModifier) {
@@ -124,7 +142,8 @@ fun ShoppingList(
     modifier: Modifier = Modifier,
     list: List<ListItem>,
     onMoveItem: (from: Int, to: Int) -> Unit,
-    onItemCheckChange: ListItem.(Boolean)->Unit
+    onItemCheckChange: ListItem.(Boolean)->Unit,
+    onEditItemClicked: (ListItem)->Unit
 ) {
     val state = rememberReorderableLazyListState(onMove = { from, to ->
         onMoveItem(from.index, to.index)
@@ -146,7 +165,9 @@ fun ShoppingList(
                         .detectReorder(state),
                     onCheckedChange = { isChecked ->
                         item.onItemCheckChange(isChecked)
-                    })
+                    },
+                    onEditItemClicked = onEditItemClicked
+                    )
             }
         }
     }
@@ -179,11 +200,13 @@ fun AddItem(
     onAddItemClicked: (String) -> Unit
 ) {
     val text = remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
 
     Row {
         TextField(
             value = text.value,
-            onValueChange = { text.value = it }
+            onValueChange = { text.value = it },
+            modifier = Modifier.focusRequester(focusRequester)
         )
         Button(onClick = {
             onAddItemClicked(text.value)
@@ -191,6 +214,51 @@ fun AddItem(
             Text("Add Item")
         }
     }
+
+    LaunchedEffect(focusRequester) {
+        focusRequester.requestFocus()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditQuantity(
+    modifier: Modifier = Modifier,
+    onChangeQuantity: (String?) -> Unit,
+    onDismiss: () -> Unit,
+) {
+    val text = remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
+
+    AlertDialog(
+        onDismissRequest = { /* Nothing */ },
+        title = {
+            Text("Enter Quantity")
+        },
+        text = {
+            TextField(
+                value = text.value,
+                onValueChange = { text.value = it },
+                modifier = Modifier.focusRequester(focusRequester)
+            )
+            LaunchedEffect(focusRequester) {
+                delay(50) //for bug https://issuetracker.google.com/issues/204502668
+                focusRequester.requestFocus()
+            }
+        },
+        confirmButton = {
+            Button(onClick = {
+                onChangeQuantity.invoke(text.value.ifBlank { null })
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    )
 }
 
 
--- app/src/main/java/com/kaaphi/yasla/model/ListItem.kt
+++ app/src/main/java/com/kaaphi/yasla/model/ListItem.kt
@@ -4,21 +4,28 @@ import android.os.Parcelable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import com.kaaphi.yasla.data.DataSource
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class ListItem(
     val name: String,
+    val quantity: String? = null,
     val isChecked: Boolean = false
 ) : Parcelable
 
 
 class ShoppingListState : ViewModel() {
     val list = mutableStateListOf<ListItem>()
+    val editItemState = MutableStateFlow<ListItem?>(null)
 
     init {
         //TODO, this needs to be async and actual load from a real place
         list.addAll(DataSource().loadList())
+    }
+
+    fun editItem(item: ListItem?) {
+        editItemState.value = item
     }
 
     fun moveItem(fromIdx: Int, toIdx: Int) = list.apply {
