cee0a6a2be198e3d49fb23096cf80f9abbee41f8
phil kaasa <phil@kaaphi.com>
2022-12-18 14:53:45 -0600

    WIP checkpoint on serialization.

--- app/src/main/java/com/kaaphi/yasla/model/ListItem.kt
+++ app/src/main/java/com/kaaphi/yasla/model/ListItem.kt
@@ -1,10 +1,17 @@
 package com.kaaphi.yasla.model
 
+import android.app.Application
 import android.os.Parcelable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.kaaphi.yasla.data.DataSource
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -15,13 +22,22 @@ data class ListItem(
 ) : Parcelable
 
 
-class ShoppingListState : ViewModel() {
+class ShoppingListState(val application: Application) : ViewModel() {
     val list = mutableStateListOf<ListItem>()
     val editItemState = MutableStateFlow<ListItem?>(null)
+    val datasource = DataSource(context = application)
+
 
     init {
-        //TODO, this needs to be async and actual load from a real place
-        list.addAll(DataSource().loadList())
+        viewModelScope.launch(Dispatchers.IO) {
+            list.addAll(datasource.loadList())
+        }
+
+        viewModelScope.launch(Dispatchers.IO) {
+            snapshotFlow { list.toList() }.flowOn(Dispatchers.IO).collect {
+                datasource.saveList(it)
+            }
+        }
     }
 
     fun editItem(item: ListItem?) {
@@ -43,5 +59,12 @@ class ShoppingListState : ViewModel() {
 
     fun addItem(itemName: String) {
         list.add(0, ListItem(itemName))
+    }
+}
+
+class ShoppingListStateFactory(private val application: Application) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return ShoppingListState(application) as T
     }
 }
